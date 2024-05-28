de5dbf757d0b72675be7ebac335aaf35e6fbb5bb
sreya-r <22bcaf62@kristujayanti.com>
2024-05-28 11:36:49 +0530

    m

--- add.c
+++ add.c
@@ -6,12 +6,10 @@ int main() {
     printf("Enter the first number: ");
     scanf("%d", &num1);
     
-    printf("Enter the second number: ");
+    ("Enter the second number: ");
     scanf("%d", &num2);
     
     sum = num1 + num3;
-    
-    printf("The sum of %d and %d is %d.", num1, num2, sum);
-    
+        
     return 0;
 }
